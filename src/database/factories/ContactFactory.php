8406d6f035ecf01cbfa2c3c8436748facf9d7e05
koko-chii <kokochii@outlook.jp>
2026-04-18 11:32:11 +0900

    インデント修正

--- src/database/factories/ContactFactory.php
+++ src/database/factories/ContactFactory.php
@@ -8,17 +8,17 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class ContactFactory extends Factory
 {
     public function definition(): array
-    {
-        return [
-            'category_id' => $this->faker->numberBetween(1, 5),
-            'first_name' => $this->faker->lastName,
-            'last_name' => $this->faker->firstName,
-            'gender' => $this->faker->numberBetween(1, 3),
-            'email' => $this->faker->safeEmail,
-            'tel' => $this->faker->phoneNumber,
-            'address' => $this->faker->address,
-            'building' => $this->faker->secondaryAddress,
-            'detail' => $this->faker->realText(200),
-        ];
-    }
+{
+    return [
+        'category_id' => fake()->numberBetween(1, 5),
+        'first_name' => fake()->lastName(),
+        'last_name' => fake()->firstName(),
+        'gender' => fake()->numberBetween(1, 3),
+        'email' => fake()->safeEmail(),
+        'tel' => fake()->phoneNumber(),
+        'address' => fake()->address(),
+        'building' => fake()->secondaryAddress(),
+        'detail' => fake()->realText(200),
+    ];
+}
 }
